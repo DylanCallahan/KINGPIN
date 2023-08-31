398dec74e95e88fac3c1736bd627b3abf013961c
DylanCallahan <wud6zc@virginia.edu>
2023-08-31 18:51:37 -0400

    added a simple for loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String[] yorick = {"big", "hulking", "massive", "yunki", "yur"};
+        int temp = 0;
+        for(int i = 0; i < yorick.length;i++) {
+            System.out.println(yorick[i]);
+            temp++;
+            System.out.println(temp);
+        }
     }
 }
